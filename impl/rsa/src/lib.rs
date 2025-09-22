a2d33d94502e9ecf8eee3197702d7d3f306920c2
arvid-persson1 <a.persson04@gmail.com>
2025-09-22 10:57:25 +0200

    more cleanup for first task code

--- impl/rsa/src/lib.rs
+++ impl/rsa/src/lib.rs
@@ -31,13 +31,13 @@ pub fn generate_key(p: &BigUint, q: &BigUint, e: BigUint) -> Result<KeySet, Keyg
     let n = p * q;
     let phi = (p - 1u8) * (q - 1u8);
 
-    if e < phi && e > BigUint::one() {
-        e.modinv(&phi)
-            .ok_or(KeygenError::InvalidExponent)
-            .map(|d| KeySet { e, d, n })
-    } else {
-        Err(KeygenError::ExponentOutOfRange)
-    }
+    (e < phi && e > BigUint::one())
+        .then(|| e.modinv(&phi))
+        .ok_or(KeygenError::ExponentOutOfRange)
+        .transpose()
+        .ok_or(KeygenError::InvalidExponent)
+        .flatten()
+        .map(|d| KeySet { e, d, n })
 }
 
 #[derive(Clone, Debug, Error, PartialEq, Eq, Hash)]
